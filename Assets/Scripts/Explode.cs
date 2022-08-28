f07965a1730051c42e676163468d0f91583f300b
makiishibashi <95393716+makiishibashi@users.noreply.github.com>
2022-08-29 00:51:35 +0900

    Score added

--- Assets/Scripts/Explode.cs
+++ Assets/Scripts/Explode.cs
@@ -6,12 +6,14 @@ public class Explode : MonoBehaviour
 {
     public GameObject explosion;
 
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.tag == "Cube")
         {
             Destroy(collision.transform.gameObject);//Destroy Cube
+            Scoring.score += 1;
             Instantiate(explosion, collision.transform.position, collision.transform.rotation);
-        }
+        }   
     }
 }
