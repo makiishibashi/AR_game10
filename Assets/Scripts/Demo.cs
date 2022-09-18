f8e5d6dcb9de2374f0cdfa7b56849cba6fb284d1
makiishibashi <95393716+makiishibashi@users.noreply.github.com>
2022-09-18 19:19:41 +0900

    Countdown timer added

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine ;
+
+public class Demo : MonoBehaviour {
+
+   [SerializeField] Timer timer ;
+   
+
+   private void Start () {
+      timer
+      .SetDuration (60)
+      .OnEnd (() => Debug.Log ("Timer 1 ended"))
+      .Begin () ;
+   }
+
+}
