897e4aa567bd0654ac52089ecdfe8af9f50cd58d
makiishibashi <95393716+makiishibashi@users.noreply.github.com>
2022-09-19 16:41:24 +0900

    Start button added

--- Assets/Scripts/Demo.cs
+++ Assets/Scripts/Demo.cs
@@ -3,13 +3,15 @@
 public class Demo : MonoBehaviour {
 
    [SerializeField] Timer timer ;
-   
 
-   private void Start () {
-      timer
+
+    //private void Start () {
+    public void StartCountdown()
+    { 
+       timer
       .SetDuration (60)
       .OnEnd (() => Debug.Log ("Timer 1 ended"))
       .Begin () ;
-   }
+    }
 
 }
